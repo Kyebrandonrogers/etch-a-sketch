06f51abea586b643d46e8887f7f37ad90a82e36a
Kyebrandonrogers <kyebrandonrogers@gmail.com>
2023-08-02 17:01:28 +0100

    grid creation works

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+function createGrid() {
+    let size = prompt("select grid size");
+    const gridContainer = document.getElementById("gridContainer");
+    for (let row = 0; row < size; row++){
+        for (let column = 0; column < size; column++){
+            const box = document.createElement("div");
+            box.classList.add("box");
+            box.style.width = `${(1/size)*100}%`;
+            box.style.height = `${(1/size)*100}%`;
+            gridContainer.appendChild(box);
+        }
+    }
+}
